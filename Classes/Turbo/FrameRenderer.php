e86a3639a8ff7ef15b7f11444cdc9777b53a5ccc
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-19 17:03:56 +0200

    FrameRenderer.php aktualisieren
    Refactoring for Typo3 v10.4

--- Classes/Turbo/FrameRenderer.php
+++ Classes/Turbo/FrameRenderer.php
@@ -9,29 +9,34 @@ class FrameRenderer
 {
     public function render(Frame $frame, string $content, ?FrameOptions $options = null, ?TopwireContext $context = null): string
     {
-        $tagBuilder = new TagBuilder('turbo-frame', $content);
+        $tagBuilder = new TagBuilder('turbo-frame');
+        $tagBuilder->setContent($content);
         $tagBuilder->addAttribute('id', $frame->id);
-        if ($context instanceof TopwireContext) {
+        
+        if ($context !== null) {
             $tagBuilder->addAttribute('data-topwire-context', $context->toHashedString());
         }
-        if ($options?->propagateUrl === true) {
-            $tagBuilder->addAttribute('data-turbo-action', 'advance');
-        }
-        if ($options?->morph === true) {
-            $tagBuilder->addAttribute('data-topwire-morph', 'true');
-        }
-        if (isset($options?->src) && $options->src !== '') {
-            $tagBuilder->addAttribute('src', $options->src);
-        }
-        if (isset($options?->target) && $options->target !== '') {
-            $tagBuilder->addAttribute('target', $options->target);
-        }
-        if (isset($options?->pageTitle) && $options->pageTitle !== '') {
-            $tagBuilder->addAttribute('data-topwire-page-title', $options->pageTitle);
-        }
-        if (isset($options?->additionalAttributes) && $options->additionalAttributes !== []) {
-            foreach ($options->additionalAttributes as $name => $value) {
-                $tagBuilder->addAttribute($name, $value);
+        
+        if ($options !== null) {
+            if ($options->propagateUrl === true) {
+                $tagBuilder->addAttribute('data-turbo-action', 'advance');
+            }
+            if ($options->morph === true) {
+                $tagBuilder->addAttribute('data-topwire-morph', 'true');
+            }
+            if ($options->src !== null && $options->src !== '') {
+                $tagBuilder->addAttribute('src', $options->src);
+            }
+            if ($options->target !== null && $options->target !== '') {
+                $tagBuilder->addAttribute('target', $options->target);
+            }
+            if ($options->pageTitle !== null && $options->pageTitle !== '') {
+                $tagBuilder->addAttribute('data-topwire-page-title', $options->pageTitle);
+            }
+            if (!empty($options->additionalAttributes)) {
+                foreach ($options->additionalAttributes as $name => $value) {
+                    $tagBuilder->addAttribute($name, $value);
+                }
             }
         }
 
