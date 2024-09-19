29566bb4e3c08737db549bda6ddc59cf1dc66927
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-19 16:56:42 +0200

    TopwireFormResponseFix.php aktualisieren
    Refactoring for Typo3 v10.4

--- Classes/Middleware/TopwireFormResponseFix.php
+++ Classes/Middleware/TopwireFormResponseFix.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Topwire\Middleware;
 
 use Psr\Http\Message\ResponseInterface;
@@ -6,21 +7,35 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-/**
- * Lightweight alternative to regular frontend requests, rendering only the provided context record/ plugin
- */
 class TopwireFormResponseFix implements MiddlewareInterface
 {
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
+        // Den RequestHandler ausführen, um die Response zu erhalten
         $response = $handler->handle($request);
-        if ($request->getMethod() !== 'POST'
-            || $response->hasHeader('Location')
-            || $request->hasHeader('Turbo-Frame')
-            || !str_contains($request->getHeaderLine('Accept'), 'text/vnd.turbo-stream.html')
-        ) {
+        
+        // Überprüfen, ob die Anforderung eine POST-Anfrage ist
+        if ($request->getMethod() !== 'POST') {
             return $response;
         }
+
+        // Überprüfen, ob der Header "Location" in der Response vorhanden ist
+        if ($response->hasHeader('Location')) {
+            return $response;
+        }
+
+        // Überprüfen, ob der Header "Turbo-Frame" im Request vorhanden ist
+        if ($request->hasHeader('Turbo-Frame')) {
+            return $response;
+        }
+
+        // Überprüfen, ob der Header "Accept" den Typ "text/vnd.turbo-stream.html" enthält
+        $acceptHeader = $request->getHeaderLine('Accept');
+        if (strpos($acceptHeader, 'text/vnd.turbo-stream.html') === false) {
+            return $response;
+        }
+
+        // Rückgabe der Response mit Status 422 und Nachricht "Form invalid"
         return $response->withStatus(422, 'Form invalid');
     }
 }
