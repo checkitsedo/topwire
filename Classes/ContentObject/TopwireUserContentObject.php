203af4f513ccc712bfe01a081cc8a4a494b94e5b
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-09-19 15:14:17 +0200

    TopwireUserContentObject.php aktualisieren
    Refactoring for Typo3 v10.4

--- Classes/ContentObject/TopwireUserContentObject.php
+++ Classes/ContentObject/TopwireUserContentObject.php
@@ -14,13 +14,23 @@ class TopwireUserContentObject extends UserContentObject
      */
     public function render($conf = [])
     {
+        // Holen der topwire-Daten aus der cObj-Elternstruktur
         $contextAndRequestFix = $this->cObj->parentRecord['data']['topwire'] ?? null;
+        
+        // Sicherstellen, dass es ein ContentObjectRenderer ist und die Daten existieren
         if ($this->cObj instanceof ContentObjectRenderer && is_array($contextAndRequestFix)) {
-            $this->request = $this->request
-                ->withAttribute('routing', $contextAndRequestFix['routing'])
-                ->withAttribute('topwire', $contextAndRequestFix['context']);
-            $this->cObj->setRequest($this->request);
+            // Setze Attribute im Request über TSFE, da direkte Request-Manipulation in v10.4 nicht unterstützt wird
+            $request = $GLOBALS['TSFE']->getRequest();
+            if ($request) {
+                $request = $request
+                    ->withAttribute('routing', $contextAndRequestFix['routing'])
+                    ->withAttribute('topwire', $contextAndRequestFix['context']);
+                $GLOBALS['TSFE']->setRequest($request); // Setze den neuen Request im TSFE
+                $this->cObj->setRequest($request); // Aktualisiere den Request im ContentObjectRenderer
+            }
         }
+
+        // Rufe das Elternrendering auf (Standardverhalten von USER)
         return parent::render($conf);
     }
 }
